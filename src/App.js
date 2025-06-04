4a7f8c3eb0a71b7509a08113c2df59419406dbee
naitik09090 <naitikraiyani88@gmail.com>
2025-06-04 11:40:50 +0530

    home

--- src/App.js
+++ src/App.js
@@ -23,10 +23,10 @@ import { Routes, Route, Navigate, useLocation } from "react-router-dom";
 // @mui material components
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-import Icon from "@mui/material/Icon";
+// import Icon from "@mui/material/Icon";
 
 // Material Dashboard 2 React components
-import MDBox from "components/MDBox";
+// import MDBox from "components/MDBox";
 
 // Material Dashboard 2 React example components
 import Sidenav from "examples/Sidenav";
